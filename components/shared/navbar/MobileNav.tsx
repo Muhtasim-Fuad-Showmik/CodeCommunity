99e9962c8891c6e7f4cc96d90c83f797e4bf7eef
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2023-12-08 03:10:22 +0600

    feat: Added a sheet menu for smaller devices

---
+++ components/shared/navbar/MobileNav.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Image from "next/image";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+const MobileNav = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Image
+          src="/assets/icons/hamburger.svg"
+          width={36}
+          height={36}
+          alt="Menu"
+          className="invert-colors sm:hidden"
+        />
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Are you sure absolutely sure?</SheetTitle>
+          <SheetDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNav;
