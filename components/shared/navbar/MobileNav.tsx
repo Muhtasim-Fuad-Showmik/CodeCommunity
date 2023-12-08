e52a309159e8efbf5a058538773b3789af40f7f4
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2023-12-09 01:37:20 +0600

    feat: Shifted sheet to the left and added elements inside

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -1,13 +1,30 @@
 import React from "react";
-import Image from "next/image";
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
+  SheetClose,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import Image from "next/image";
+import Link from "next/link";
+import { SignedOut } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { sidebarLinks } from "@/constants";
+
+const NavContent = () => {
+  return (
+    <section className="flex h-full flex-col gap-6 pt-16">
+      {sidebarLinks.map((item) => (
+        <SheetClose asChild key={item.route}>
+          <Link href={item.route} className="">
+            <Image src={item.imgURL} alt={item.label} width={20} height={20} />
+            <p>{item.label}</p>
+          </Link>
+        </SheetClose>
+      ))}
+    </section>
+  );
+};
 
 const MobileNav = () => {
   return (
@@ -21,14 +38,42 @@ const MobileNav = () => {
           className="invert-colors sm:hidden"
         />
       </SheetTrigger>
-      <SheetContent>
-        <SheetHeader>
-          <SheetTitle>Are you sure absolutely sure?</SheetTitle>
-          <SheetDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </SheetDescription>
-        </SheetHeader>
+      <SheetContent
+        side="left"
+        className="background-light900_dark200 border-none"
+      >
+        <Link href="/" className="flex items-center gap-1 pb-4">
+          <Image
+            src="/assets/images/code-community-logo.png"
+            width={70}
+            height={23}
+            alt="CodeCommunity"
+          />
+        </Link>
+        <div>
+          <SheetClose asChild>
+            <NavContent />
+          </SheetClose>
+        </div>
+
+        <SignedOut>
+          <div className="flex flex-col gap-3">
+            <SheetClose asChild>
+              <Link href="/sign-in">
+                <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                  <span className="primary-text-gradient">Log In</span>
+                </Button>
+              </Link>
+            </SheetClose>
+            <SheetClose asChild>
+              <Link href="/sign-up">
+                <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                  Sign Up
+                </Button>
+              </Link>
+            </SheetClose>
+          </div>
+        </SignedOut>
       </SheetContent>
     </Sheet>
   );
